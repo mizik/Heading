87ec958e80feb7e0f1ed5539f586945d6ce9bdae
Mráza <Mrza@Mraza-PC>
2013-04-30 14:37:03 +0200

    Inicializace

---
+++ public_html/js/main.js
@@ -0,0 +1,8 @@
+(function(){
+    var wrapper = $('#wrapper'),
+	h1 = $('#origin');
+    $('<h1>'+h1.text()+'</h1>').appendTo('#wrapper');
+    $('<h1>'+h1.text()+'</h1>').appendTo('#wrapper');
+    $('<h1>'+h1.text()+'</h1>').appendTo('#wrapper');
+    $('<h1>'+h1.text()+'</h1>').appendTo('#wrapper');    
+})();
